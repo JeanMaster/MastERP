6fcb3d6aaf633836ce8a0067a4bafa9558766a5d
Jean Desktop <jeancarlos70@gmail.com>
2026-05-27 10:19:52 -0400

    render bug db

--- apps/backend/test-env.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-    const count = await prisma.purchase.count();
-    process.stdout.write('PURCHASE_COUNT:' + count + '\n');
-}
-main().catch(err => {
-    process.stderr.write('ERROR:' + err.message + '\n');
-}).finally(() => prisma.$disconnect());
